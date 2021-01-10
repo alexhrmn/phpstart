5ed9658a8e4b2e87b22165b98bf28d9f3916fff5
alexhrmn <alxhirman1993@gmail.com>
2021-01-10 19:15:16 +0200

    end of create MVC well done!!!!

--- models/News.php
+++ models/News.php
@@ -14,16 +14,18 @@ class News
          $id = intval ($id);
 
          if ($id) {
-           $host = 'localhost';
-           $dbname = 'mvc_site';
-           $user = 'root';
-           $password = 'password';
-           $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password,);
+           // Заменил на getConnection
+           // $host = 'localhost';
+           // $dbname = 'mvc_site';
+           // $user = 'root';
+           // $password = 'password';
+           // $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+
+           $db = Db::getConnection();
 
            $result = $db->query('SELECT * from news WHERE id=' . $id);
 
-
-
+           $result->setFetchMode(PDO::FETCH_ASSOC);
 
            $newsItem = $result->fetch();
 
@@ -43,31 +45,22 @@ class News
     {
 
          // Запрос к БД
+         $db = Db::getConnection();
 
+         $newsList = array();
 
-      $host = 'localhost';
-      $dbname = 'mvc_site';
-      $user = 'root';
-      $password = 'password';
-      $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-
-      $newsList = array();
-
-      $result = $db->query('SELECT id, title, date, short_content, type '
+         $result = $db->query('SELECT id, title, date, short_content '
               . 'FROM news '
               . 'ORDER BY date DESC '
               . 'LIMIT 10 ');
 
       $i = 0;
 
-        while($row = $result->fetch())
-
-      {
-        $newsList[$i]['id'] = $row['id'];
-        $newsList[$i]['title'] = $row['title'];
-        $newsList[$i]['date'] = $row['date'];
-        $newsList[$i]['short_content'] = $row['short_content'];
-        $newsList[$i]['type'] = $row['type'];
+        while($row = $result->fetch()){
+          $newsList[$i]['id'] = $row['id'];
+          $newsList[$i]['title'] = $row['title'];
+          $newsList[$i]['date'] = $row['date'];
+          $newsList[$i]['short_content'] = $row['short_content'];
 
         $i++;
       }
